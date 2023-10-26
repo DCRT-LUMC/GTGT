489f7a07e547c7ecb714e71a917af860450a9ed7
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2023-10-26 08:13:59 +0200

    Implement intersections using Python sets

--- src/GTGT/bed.py
+++ src/GTGT/bed.py
@@ -1,4 +1,4 @@
-from typing import Optional, Iterator, List, Tuple, Union
+from typing import Optional, Iterator, List, Tuple, Union, Set
 
 # Int, or a string we can cast to int
 castable_int = Union[int, str]
@@ -122,31 +122,39 @@ class Bed:
 
     Range = Tuple[int, int]
     @staticmethod
-    def _intersect(a: Range, b: Range) -> Optional[Range]:
-        """Determine the intersection between two ranges"""
+    def _intersect(a: Range, b: Range) -> List[Range]:
+        """Determine the intersection between two ranges
+
+        This is lazy implementation, where we create all numbers in a range,
+        and use python sets to find the intersections.
+
+        """
         intersect = set(range(*a)).intersection(set(range(*b)))
         return _to_range(intersect)
 
 Range = Tuple[int, int]
-def _to_range(numbers: List[int]) -> List[Range]:
+def _to_range(numbers: Set[int]) -> List[Range]:
     """Convert a range of numbers to [start, end)"""
-    # If there are no numbers
-    if not numbers:
+    # Make sure the numbers are sorted
+    _numbers = sorted(numbers)
+
+    # If there are no _numbers
+    if not _numbers:
         return list()
 
     # If there is only a single number
-    if len(numbers) == 1:
-        i = numbers[0]
+    if len(_numbers) == 1:
+        i = _numbers[0]
         return [(i, i+1)]
 
     # Initialise the start and previous number
-    start = prev = numbers[0]
+    start = prev = _numbers[0]
 
     # Store the ranges we found
     ranges = list()
 
-    # Process all numbers
-    for i in numbers[1:]:
+    # Process all _numbers
+    for i in _numbers[1:]:
         if i == prev + 1:
             prev = i
         else:
--- tests/test_bed.py
+++ tests/test_bed.py
@@ -130,28 +130,30 @@ def test_bed_roundtrip(bed: Bed, line: str) -> None:
 
 
 intersect = [
-    ((0, 10), (10, 20), None),
-    #((0, 10), (0, 10), (0, 10)),
+    # Range a, range b, intersection
+    ((0, 10), (10, 20), list()),
+    ((0, 10), (0, 10), [(0, 10)]),
 ]
 
 Range = Tuple[int, int]
 @pytest.mark.parametrize("a, b, intersection", intersect)
-def test_intersect_ranges(a: Range, b: Range, intersection: Range) -> None:
+def test_intersect_ranges(a: Range, b: Range, intersection: List[Range]) -> None:
     assert Bed._intersect(a, b) == intersection
 
 to_range = [
     ([], []),
     ([0], [(0, 1)]),
     ([0, 1], [(0, 2)]),
-    ([0, 1, 2, 4, 5], [(0, 3), (4, 6)])
+    ([0, 1, 2, 4, 5], [(0, 3), (4, 6)]),
+    ([3, 2, 1, 0], [(0, 4)])
 ]
 @pytest.mark.parametrize("numbers, expected", to_range)
 def test_to_range(numbers: List[int], expected: List[Range]) -> None:
     # Test from numbers to ranges
-    assert _to_range(numbers) == expected
+    assert _to_range(set(numbers)) == expected
 
     # Test from returned ranges to numbers
     nums = list()
     for r in expected:
         nums+=list(range(*r))
-    assert nums == numbers
+    assert set(nums) == set(numbers)
