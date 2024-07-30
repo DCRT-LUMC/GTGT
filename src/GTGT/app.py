9fd7a73e39a8bc594a2d2073f8340a5abfdf884b
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2024-07-30 09:30:58 +0200

    Add function description

--- src/GTGT/app.py
+++ src/GTGT/app.py
@@ -15,9 +15,11 @@ provider = Provider()
 
 @app.get("/links/{variant}")
 async def get_links(variant: str) -> Dict[str, str]:
+    """Lookup external references for the specified variant"""
     return lookup_variant(provider, variant).url_dict()
 
 
 @app.get("/transcript/{transcript_id}")
 async def get_transcript(transcript_id: str) -> TranscriptModel:
+    """Lookup the specified transcript"""
     return lookup_transcript(provider, transcript_id)
