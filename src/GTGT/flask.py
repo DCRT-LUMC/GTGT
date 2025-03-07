885bed9e56c19b8d22c6cd5c507a03ad449d9a13
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2025-03-07 06:36:31 +0100

    Analyze the transcript in flask

--- src/GTGT/flask.py
+++ src/GTGT/flask.py
@@ -1,6 +1,10 @@
+from GTGT.variant_validator import lookup_variant
+from .provider import Provider
+from .wrappers import lookup_transcript
 from flask import Flask, render_template
 
 app = Flask(__name__)
+provider = Provider()
 
 @app.route("/")
 def home():
@@ -8,4 +12,17 @@ def home():
 
 @app.route("/<variant>")
 def result(variant):
-    return render_template("report.html")
+    if not variant:
+        return render_template("report.html")
+
+    # Analyze the transcript
+    transcript_id = variant.split(":")[0]
+    transcript_model = lookup_transcript(provider, transcript_id)
+    transcript = transcript_model.to_transcript()
+    results = transcript.analyze(variant)
+
+    # Get external links
+    links = lookup_variant(provider, variant).url_dict()
+
+    print(results)
+    return render_template("report.html", results=results, links=links, variant=variant)
