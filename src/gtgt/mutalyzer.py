7625dabcb182c917ef6649dca693088c66dab293
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2025-07-22 10:26:57 +0200

    Remove HGVS_to_genome_range

--- src/gtgt/mutalyzer.py
+++ src/gtgt/mutalyzer.py
@@ -324,27 +324,6 @@ class HGVS(BaseModel):
                 raise NotImplementedError(msg)
 
 
-def HGVS_to_genome_range(d: Description) -> Tuple[int, int]:
-    """Convert HGVS variant description to affected genome range
-
-    NOTE that the genome range is similar to the UCSC annotations on the genome,
-    i.e. 0 based, half open. Not to be confused with hgvs g. positions
-    """
-    d.normalize()
-    model = d.ensembl_model_with_no_offset()
-
-    if len(model["variants"]) == 0:
-        raise ValueError("Descriptions without variants are not supported")
-    if len(model["variants"]) > 1:
-        raise ValueError("Multiple variants are not supported")
-
-    # Get start and end of the description
-    start = model["variants"][0]["location"]["start"]["position"]
-    end = model["variants"][0]["location"]["end"]["position"]
-
-    return (start, end)
-
-
 def combine_variants_deletion(
     variants: Sequence[Variant], deletion: Variant
 ) -> List[Variant]:
