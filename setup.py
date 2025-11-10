8e9d2fc46ea3543c036ca7fef8d8929f8d8ebcf3
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2025-11-10 09:58:30 +0100

    Drop support for python 3.9

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         'Operating System :: Unix',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
@@ -57,7 +56,7 @@ setup(
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.9',
+    python_requires='>=3.10',
     install_requires=[
         "pydantic",
         "setuptools",
