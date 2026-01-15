e39f81e8d6eec1448b7d99cb42aaaa7efe16876e
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2026-01-15 14:54:31 +0100

    Revert "Hotfix to temporary disable protein domains"
    This reverts commit 4e3884d5bb629799b4d463c830178c7ede4f884f.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ project = 'GTGT'
 copyright = '2023, Redmar van den Berg'
 author = 'Redmar van den Berg'
 
-release = '0.2.11'
+release = '0.2.10'
 version = '.'.join(release.split('.')[0:2])
 
 # -- General configuration
