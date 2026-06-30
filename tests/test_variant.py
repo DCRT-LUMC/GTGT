ef6a7e71a43adb82d67e13ac26996ff97fe166f5
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2026-06-30 14:26:03 +0200

    Variant.genomic_coordinates has been deprecated

--- tests/test_variant.py
+++ tests/test_variant.py
@@ -664,21 +664,24 @@ class TestVariantMutalyzerForward(object):
         variant_model = Variant.from_model(delins_model, sequence=seq)
         assert variant_model == variant
 
-    COORDINATES = [
-        (Variant(47, 50), (112_086_919, 112_086_922)),
-        (Variant(53, 56), (112_086_925, 112_086_928)),
-        (Variant(44, 8095), (112_086_916, 112_094_967)),
-        (Variant(47, 56), (112_086_919, 112_086_928)),
-        (Variant(47, 8095), (112_086_919, 112_094_967)),
-    ]
-
-    @pytest.mark.parametrize("variant, genomic_coordinates", COORDINATES)
-    def test_Variant_to_genomic_coordinates(
-        self, variant: Variant, genomic_coordinates: tuple[int, int]
-    ) -> None:
-        """Test converting Variant coordinates to genomic coordinates"""
-        d = init_description(f"{self.transcript}:c.=")
-        assert variant.genomic_coordinates(d) == genomic_coordinates
+    # the function genomic_coordinates has been removed from the Variant class
+    # Keep this test around for now, and later convert the test case to test the genomic crossmapper
+    # (It was a lot of work to manually generate and verify the test cases)
+    # COORDINATES = [
+    #     (Variant(47, 50), (112_086_919, 112_086_922)),
+    #     (Variant(53, 56), (112_086_925, 112_086_928)),
+    #     (Variant(44, 8095), (112_086_916, 112_094_967)),
+    #     (Variant(47, 56), (112_086_919, 112_086_928)),
+    #     (Variant(47, 8095), (112_086_919, 112_094_967)),
+    # ]
+    #
+    # @pytest.mark.parametrize("variant, genomic_coordinates", COORDINATES)
+    # def test_Variant_to_genomic_coordinates(
+    #     self, variant: Variant, genomic_coordinates: tuple[int, int]
+    # ) -> None:
+    #     """Test converting Variant coordinates to genomic coordinates"""
+    #     d = init_description(f"{self.transcript}:c.=")
+    #     assert variant.genomic_coordinates(d) == genomic_coordinates
 
 
 class TestVariantMutalyzerReverse(TestVariantMutalyzerForward):
@@ -927,19 +930,22 @@ class TestVariantMutalyzerReverse(TestVariantMutalyzerForward):
         variant_model = Variant.from_model(delins_model, sequence=seq)
         assert variant_model == variant
 
-    COORDINATES = [
-        (Variant(47571, 47574), (32_435_345, 32_435_348)),
-        (Variant(1286, 47577), (32_389_060, 32_435_351)),
-        (Variant(47565, 47577), (32_435_339, 32_435_351)),
-        (Variant(47565, 47577), (32_435_339, 32_435_351)),
-        (Variant(47568, 47577), (32_435_342, 32_435_351)),
-        (Variant(1286, 47502), (32_389_060, 32_435_276)),
-    ]
-
-    @pytest.mark.parametrize("variant, genomic_coordinates", COORDINATES)
-    def test_Variant_to_genomic_coordinates(
-        self, variant: Variant, genomic_coordinates: tuple[int, int]
-    ) -> None:
-        """Test converting Variant coordinates to genomic coordinates"""
-        d = init_description(f"{self.transcript}:c.=")
-        assert variant.genomic_coordinates(d) == genomic_coordinates
+    # the function genomic_coordinates has been removed from the Variant class
+    # Keep this test around for now, and later convert the test case to test the genomic crossmapper
+    # (It was a lot of work to manually generate and verify the test cases)
+    # COORDINATES = [
+    #     (Variant(47571, 47574), (32_435_345, 32_435_348)),
+    #     (Variant(1286, 47577), (32_389_060, 32_435_351)),
+    #     (Variant(47565, 47577), (32_435_339, 32_435_351)),
+    #     (Variant(47565, 47577), (32_435_339, 32_435_351)),
+    #     (Variant(47568, 47577), (32_435_342, 32_435_351)),
+    #     (Variant(1286, 47502), (32_389_060, 32_435_276)),
+    # ]
+    #
+    # @pytest.mark.parametrize("variant, genomic_coordinates", COORDINATES)
+    # def test_Variant_to_genomic_coordinates(
+    #     self, variant: Variant, genomic_coordinates: tuple[int, int]
+    # ) -> None:
+    #     """Test converting Variant coordinates to genomic coordinates"""
+    #     d = init_description(f"{self.transcript}:c.=")
+    #     assert variant.genomic_coordinates(d) == genomic_coordinates
