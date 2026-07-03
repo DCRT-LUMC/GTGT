4d84966e4edb94c5fb0b92b2de51cf22ae998cb9
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2026-07-03 09:19:53 +0200

    Analyze the NM transcripts without protein domains

--- t.sh
+++ t.sh
@@ -3,10 +3,10 @@ set -e
 
 
 # WT1="ENST00000452863.10:c.100del ENST00000452863.10:r.100del NC_000011.10(NM_024426.6):c.100del NC_000011.10(NM_024426.6):r.100del"
-WT1="ENST00000452863.10:c.100del NC_000011.10(NM_024426.6):c.100del"
+# WT1="ENST00000452863.10:c.100del NC_000011.10(NM_024426.6):c.100del"
 
 # SDHD="ENST00000375549.8:c.100del ENST00000375549.8:r.100del NC_000011.10(NM_003002.4):c.100del NC_000011.10(NM_003002.4):r.100del"
-SDHD="ENST00000375549.8:c.100del NC_000011.10(NM_003002.4):c.100del"
+# SDHD="ENST00000375549.8:c.100del NC_000011.10(NM_003002.4):c.100del"
 
 if [ ! $# -eq 1 ]; then
   echo "Usage: $0 name"
@@ -20,9 +20,22 @@ mkdir -p ${name}
 
 # for transcript in $(python3 crossmapper.py | grep ":c." | grep -v "^NM"); do
 # for transcript in $(python3 crossmapper.py | grep ":r." | grep -v "^NM"); do
-for transcript in $(python3 crossmapper.py | grep -v "^NM"); do
-  echo "############### analyzing $transcript ###############"
-  gtgt export ${transcript} --protein > ${name}/${transcript}.txt
+# for transcript in $(python3 crossmapper.py | grep -v "^NM"); do
+for transcript in $(python3 crossmapper.py); do
+  fname=${name}/${transcript}.txt
+
+  if [ -f ${fname} ];then
+    echo "Skipping ${transcript}, ${fname} already exists"
+    continue
+  fi
+  echo "Analyzing $transcript"
+
+  if [[ ${transcript} == NM* ]]; then
+    # No protein annotations for naked NM
+    gtgt analyze ${transcript} --extended > ${fname}
+  else
+    gtgt analyze ${transcript} --protein --extended > ${fname}
+  fi
 done
 
 exit
