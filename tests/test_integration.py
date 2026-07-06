4c2151c21a405da9113e8215a4fddf819280786a
Redmar van den Berg <RedmarvandenBerg@lumc.nl>
2026-07-06 10:10:20 +0200

    Test different transcript identifiers for the same variant
    ENST, NM and NC(NM) identifiers for the same transcript have different
    internal coordinate systems, which GTGT has to play nice with.

---
+++ tests/test_integration.py
@@ -0,0 +1,89 @@
+from typing import Sequence
+
+import pytest
+
+from gtgt import Transcript
+from gtgt.mutalyzer import init_description, sequence_from_description
+from gtgt.transcript import Comparison
+from gtgt.variant import Variant
+
+
+def compare_to_wildtype(hgvs: str) -> Sequence[Comparison]:
+    """
+    Helper function to build a transcript from the supplied HGVS, and compare it to the wildtype transcript
+    """
+    # First, create the wildtype transcript
+    *rest, variant = hgvs.split(".")
+    wt_hgvs = ".".join(rest) + ".="
+
+    wt = Transcript.from_description(init_description(wt_hgvs))
+
+    # Next, we create and mutate the transcript of interest
+    d = init_description(hgvs)
+    t = Transcript.from_description(d)
+
+    sequence = sequence_from_description(d)
+    input_variants = [
+        Variant.from_model(delins, sequence=sequence)
+        for delins in d.delins_model["variants"]
+    ]
+    t.mutate(d, input_variants)
+
+    return t.compare(wt)
+
+
+class TestDifferentTranscripts:
+    """Class to test the same mutation on different transcripts
+    (with different internal coordinate systems)
+    """
+
+    common_args = ("coordinate", ["c", "r"])
+
+    # NOTE: These variants are special, they are all deletions of one or more full amino acids
+    SDHD_variants = [
+        # Deletion in exon 1
+        "31_33del",
+        # Deletion across exon 1 and 2
+        "49_57del",
+        # Deletion in exon 2
+        "100_132del",
+        # Deletion in the last exon
+        "451_453del",
+        # Multiple variants
+        "[31_33del;451_453del]",
+    ]
+
+    @pytest.mark.slow
+    @pytest.mark.parametrize(*common_args)
+    @pytest.mark.parametrize("variant", SDHD_variants)
+    def test_different_transcripts_SDHD(self, coordinate: str, variant: str) -> None:
+        """Test that ENST, NC(NM) and NM variants are handled the same
+
+        Because the internal coordinate systems differ we cannot compare these
+        directly but we have to compare them via the comparison with the wildtype
+        version of the corresponding transcript
+
+        Test this for both r. and c., since r. descriptions on a given transcript
+        have different internal coordinates the  c.
+        """
+        SDHD = ["ENST00000375549.8", "NC_000011.10(NM_003002.4)", "NM_003002.4"]
+
+        # First, we compare ENST to NC(NM)
+        enst, nm = SDHD[0], SDHD[-1]
+        hgvs_enst = f"{enst}:{coordinate}.{variant}"
+        hgvs_nm = f"{nm}:{coordinate}.{variant}"
+
+        cmp_enst = compare_to_wildtype(hgvs_enst)
+        cmp_nm = compare_to_wildtype(hgvs_nm)
+
+        assert cmp_enst == cmp_nm
+
+        # Next, we compare ENST to the bare NM
+        enst, nm = SDHD[:2]
+        hgvs_enst = f"{enst}:{coordinate}.{variant}"
+        hgvs_nm = f"{nm}:{coordinate}.{variant}"
+
+        cmp_enst = compare_to_wildtype(hgvs_enst)
+        cmp_nm = compare_to_wildtype(hgvs_nm)
+
+        assert cmp_enst == cmp_nm
